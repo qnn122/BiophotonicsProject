db6ddb77bb0f55970a10dc79cb1dc3ebd52da51e
quangNguyen122 <bme.nnquang@gmail.com>
2015-11-20 08:40:51 +0700

    Add file read analog and send digital signal for arduino

---
+++ readAnalog.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);               // read the input on analog pin 0:
+  float voltage = sensorValue * (5.0 / 1023.0);   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  Serial.println(voltage);                        // print out the value you read:
+  
+  delay(1);
+}
