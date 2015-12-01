8eb34d1d978988abd7fa0f8f5d892b2c442f01c2
quangNguyen122 <bme.nnquang@gmail.com>
2015-12-01 14:06:35 +0700

    Set up graphView

--- BluetoothConnect/app/src/main/java/com/example/quangnguyen/bluetoothconnect/MainActivity.java
+++ BluetoothConnect/app/src/main/java/com/example/quangnguyen/bluetoothconnect/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.quangnguyen.bluetoothconnect;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
-import android.widget.LinearLayout;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
-import android.content.Intent;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button bConnect, bDisconnect, bXminus, bXplus;
     ToggleButton tbLock, tbScroll, tbStream;
     static boolean Lock, AutoScroll, Stream;
+    // Graph init()
+    static LinearLayout GraphView;
+    static GraphView graphView;
+    static GraphViewSeries Series;
+    private static double graph2LastXvalue = 0;
+    private static int Xview = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +46,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void init() {
+        GraphView = (LinearLayout) findViewById(R.id.Graph);
+        Series = new GraphViewSeries("Signal",
+                    new GraphViewSeries.GraphViewStyle(Color.YELLOW,2),
+                    new GraphView.GraphViewData[] {new GraphView.GraphViewData(0,0)});
+        graphView = new LineGraphView(this, "Graph");
 
+        graphView.setViewPort(0, Xview);
+        graphView.setScrollable(true);
+        graphView.setScalable(true);
+        graphView.setShowLegend(true);
+        graphView.setLegendAlign(com.jjoe64.graphview.GraphView.LegendAlign.BOTTOM);
+        graphView.setManualYAxis(true);
+        graphView.setManualYAxisBounds(5, 0);
+        graphView.addSeries(Series);
+        graphView.addView(graphView);
     }
 
     void Buttoninit() {
