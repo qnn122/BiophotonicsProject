9f8b1bea2794831ae1165c2e37cba96bb46ab186
quangNguyen122 <bme.nnquang@gmail.com>
2015-12-16 10:30:50 +0700

    Add timer
    Run when click start. But can't stop

--- Arduino/AndroidConnect/AndroidConnect.ino
+++ Arduino/AndroidConnect/AndroidConnect.ino
@@ -1,6 +1,35 @@
+//Avr-libc library includes
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+bool FLAG = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // initiate Timer1
+  cli();      //disable global interrupts
+  TCCR1A = 0; //set entire TCCR1A register to 0
+  TCCR1B = 0;
+
+  // Set compare match register to desired timer count:
+  //OCR1A = 1999;   // fs = 100Hz, 10ms
+  OCR1A = 3999;   // fs = 50Hz, 20ms
+
+  // Turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  
+  // Set CS10 and CS12 bits for 8 prescaler;
+  TCCR1B |= (1 << CS11); // clock/8
+
+  // enable Timer1 overflow or compare interrupt
+  //TIMSK1 = (1 << TOIE1);     // TIMSK = Timer/Counter Interrupt Mask Register
+                            // Set TOIE1 -> 1: tells the timer to trigger an interrupt when the timer overflows
+  TIMSK1 = (1 << OCIE1A);   // Compare interrupt
+
+  // Enable global interrupts
+  sei();
+                        
 }
 
 void loop() {
@@ -10,32 +39,28 @@ void loop() {
 
     switch(re) {
       case 'E':
-      start();
+        // interrupt function here
+        FLAG = true; 
       break;
     }
   }
+  
+}   // loop()
 
-}
-
-/*Send signal through serial port continuously*/
-void start() {
-  while(1) {
+ISR(TIMER1_COMPA_vect) {
+  if (FLAG) {
     Serial.print('s');
     //Serial.print(mapping(analogRead(A0), 0, 1023, 0, 5),2);
     int value = analogRead(A0);
     float sendvalue = value*5/1023;
     Serial.println(sendvalue);
-    delay(20);
-
+    
     if (Serial.available() > 0) {
-      if (Serial.read() == 'Q') return; // leave start() when receive 'Q' command
+      if (Serial.read() == 'Q') {
+        FLAG = false;
+        return; // leave start() when receive 'Q' command;
+      }
     }
-  }
-}
-
-/*Map analog signal
-float mapping(float x, float inMin, float inMax, float outMin, float outMax) {
-  return (x-inMin)*(outMax - outMin)/(inMax - inMin) + outMin;
-}
-*/
+  } // if FLAG
+} // interrupt
 
